99c21ed22eda674a3e9a9c8f4d01132319208ce1
Amy <awshu1997@gmail.com>
2014-06-20 21:04:33 -0700

    added Driver to models

--- server/dispatcher/models.py
+++ server/dispatcher/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Driver(models.Model): 
+	name = models.CharField(max_length=30)
+	employnum = models.IntegerField()
+	picture = models.ImageField()
+	lati = models.FloatField()
+	longi = models.FloatField()
