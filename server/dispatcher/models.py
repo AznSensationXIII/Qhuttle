4ef31ba124d534a0a5a4a5ded3f96ad3672d83ac
AznSensationXIII <brandon.eusebio@gmail.com>
2014-06-20 22:29:51 -0700

    Progress on views

--- server/dispatcher/models.py
+++ server/dispatcher/models.py
@@ -2,18 +2,26 @@ from django.db import models
 
 # Create your models here.
 class Driver(models.Model): 
-	name = models.CharField(max_length=30)
-	employnum = models.IntegerField()
-	picture = models.ImageField()
-	lati = models.FloatField()
-	longi = models.FloatField()
+    name = models.CharField(max_length=30)
+    employnum = models.IntegerField()
+    # picture = models.ImageField()
+    lati = models.FloatField()
+    longi = models.FloatField()
+
+    def to_dict(self):
+        return { 'name' : self.name,
+                 'employee_number' : self.employnum,
+                 'latitude' : self.lati,
+                 'longitude' : self.longi }
 
 class Passenger(models.Model):
-	name = models.CharField(max_length=30)
-	num_pass = models.IntegerField(default=1)
-	loc_pickup = models.CharField(max_length=100)
-	loc_drop = models.CharField(max_length=100)
-	req_time = models.TimeField('Request Time')
-	asap_flag = models.BooleanField(default=False)
-	drive = models.CharField(max_length=30, default='')
-	push_id = models.IntegerField(default=-1)
+    name = models.CharField(max_length=30)
+    emp_num = models.IntegerField(default=-1)
+    num_pass = models.IntegerField(default=1)
+    loc_pickup = models.CharField(max_length=100)
+    loc_drop = models.CharField(max_length=100)
+    req_time = models.TimeField('Request Time')
+    asap_flag = models.BooleanField(default=False)
+    driver_num = models.IntegerField(default=-1)
+    push_id = models.IntegerField(default=-1)
+
--- server/dispatcher/views.py
+++ server/dispatcher/views.py
@@ -1,3 +1,65 @@
+from dispatcher.models import Driver, Passenger
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.core.serializers.json import DjangoJSONEncoder
+import json
 
 # Create your views here.
+
+def add_driver(request):
+    if request.method == 'POST':
+        try: 
+            info = json.loads(request.body)
+            driver = Driver( name=info['name'],
+                             employnum=info['employee_number'],
+                             lati=info['latitude'],
+                             longi=info['longitude'] )
+            driver.save()
+            return HttpResponse(status=200)
+        except KeyError:
+            return HttpResponse(status=400)
+
+def remove_driver(request):
+    if request.method == "POST":
+        try:
+            info = json.loads(request.body)
+            driver = Driver.objects.get(e_num=info['employee_number'])
+            driver.delete()
+            return HttpResponse(status=200)
+        except Dispatcher.DoesNotExist:
+            return HttpResponse(status=400)
+        except KeyError:
+            return HttpResponse(status=400)
+
+def enqueue(request):
+    if request.method == 'POST':
+        try:
+            info = json.loads(request.body)
+            passenger = Passenger( name=info['name'],
+                                   emp_num=info['employee_number']
+                                   num_pass=info['num_passengers'],
+                                   loc_pickup=info['location_pickup'],
+                                   loc_drop=info['location_dropoff'],
+                                   req_time=info['request_time'],
+                                   asap_flag=info['asap_flag'],
+                                   driver_num=info['driver_number'],
+                                   push_id=info['push_id'] )
+            passenger.save()
+            return HttpResponse(status=200)
+        except KeyError:
+            return HttpResponse(status=400)
+
+def dequeue(request):
+    if request.method == 'POST':
+        try:
+            info = json.loads(request.body)
+            passenger = Passenger.objects.get(emp_num=info['employee_number'])
+            if passenger.driver_num == -1:
+                passenger.driver_num = info['driver_num']
+
+        except Dispatcher.DoesNotExist:
+            return HttpResponse(status=400)
+        except KeyError:
+            return HttpResponse(status=400)
+
+
