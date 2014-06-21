e0f005f9bb0574c8d724379ef07b266c25d481df
AznSensationXIII <brandon.eusebio@gmail.com>
2014-06-20 21:06:50 -0700

    Added driver model

--- server/dispatcher/models.py
+++ server/dispatcher/models.py
@@ -7,3 +7,13 @@ class Driver(models.Model):
 	picture = models.ImageField()
 	lati = models.FloatField()
 	longi = models.FloatField()
+
+class Passenger(models.Model):
+	name = models.CharField(max_length=30)
+	num_pass = models.IntegerField(default=1)
+	loc_pickup = models.CharField(max_length=100)
+	loc_drop = models.CharField(max_length=100)
+	req_time = models.TimeField('Request Time')
+	asap_flag = models.BooleanField(default=False)
+	drive = models.CharField(max_length=30, default='')
+	push_id = models.IntegerField(default=-1)
